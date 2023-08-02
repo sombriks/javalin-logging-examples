34b3d8a23a02074c4ee0ea8a5c6066eef925d893
Leonardo Silveira <sombriks@gmail.com>
2023-08-02 02:27:13 -0300

    small logging samples with javalin

---
+++ 01-no-logging-error/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import io.javalin.Javalin;
+
+public class App {
+
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
